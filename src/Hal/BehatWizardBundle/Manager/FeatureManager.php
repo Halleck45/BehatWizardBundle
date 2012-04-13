41a057d2192c49a18dee90dcf0ad50c43b3a3a30
Jean-François Lépine <jeanfrancois@lepine.pro>
2012-04-13 19:40:35 +0200

    fetch one or more features by its manager

--- src/Hal/BehatWizardBundle/Manager/FeatureManager.php
+++ src/Hal/BehatWizardBundle/Manager/FeatureManager.php
@@ -1,7 +1,47 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+namespace Hal\BehatWizardBundle\Manager;
+
+use Doctrine\ORM\EntityManager,
+    Doctrine\Common\Collections\Collection;
+use Hal\BehatWizardBundle\Entity\Feature,
+    Hal\BehatWizardBundle\Entity\Scenario,
+    Hal\BehatWizardBundle\Entity\State;
+
+class FeatureManager
+{
+
+    protected $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    /**
+     * Load feature by its ID
+     * 
+     * @param integer $id
+     * @return Hal\BehatWizardBundle\Entity\Feature 
+     */
+    public function loadFeature($id)
+    {
+        return $this->getRepository()->find($id);
+    }
+
+    /**
+     * Get the list of available features (from database)
+     * 
+     * @return Doctrine\Common\Collections\Collection
+     */
+    public function getFeatures()
+    {
+        return $this->getRepository()->findAll();
+    }
+
+    public function getRepository()
+    {
+        return $this->em->getRepository('BehatWizardBundle:Favorite');
+    }
+
+}
