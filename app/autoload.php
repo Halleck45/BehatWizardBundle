372cf91cd539fa3811e1a17d4ba667ba892a9a8f
Jean-François Lépine <jeanfrancois@lepine.pro>
2012-04-22 19:51:53 +0200

    add gherkin dependency

--- app/autoload.php
+++ app/autoload.php
@@ -15,6 +15,7 @@ $loader->registerNamespaces(array(
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
     'FOS'              => __DIR__.'/../vendor/bundles',
+    'Behat\\Gherkin'   => __DIR__.'/../vendor/behat/gherkin/src',
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
