0be71c563c10a640b6157d139ffe6bcc5bb25693
Jean-François Lépine <jeanfrancois@lepine.pro>
2012-04-22 16:47:22 +0200

    importing js route bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Hal\BehatWizardBundle\HalBehatWizardBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
